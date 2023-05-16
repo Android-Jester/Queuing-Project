8bc2b8860046fcc96f60a0042df2ceefc6149cf0
Duah Kwadwo Adjei <duah14@outlook.com>
2023-05-16 10:27:25 +0000

    more ignore

--- queuing_server/src/main.rs
+++ queuing_server/src/main.rs
@@ -1,26 +1,14 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-use log::info;
-use simple_logger;
-mod features;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    info!("Hello There sorry for the trouble");
+mod core;
+mod data_sources;
+mod domain;
+mod presentation;
 
-    HttpResponse::Accepted().body(String::from("Wrong way buddy"))
-}
 
-#[actix_web::main]
+use presentation::start_server;
+
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    simple_logger::init_with_level(log::Level::Info).unwrap();
-    info!("Server Starting . . . . .");
-
-    HttpServer::new(move || {
-        info!("Server Service Beginning . . . . .");
-        App::new()
-            .service(hello)
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    start_server().unwrap();
+    Ok(())
 }
--- queuing_server/target/debug/deps/bank_queue_project-441e5fee64634374.d
+++ queuing_server/target/debug/deps/bank_queue_project-441e5fee64634374.d
@@ -1,7 +1,10 @@
-/home/android-jester/Documents/Bank-Queuing-System-Project/target/debug/deps/bank_queue_project-441e5fee64634374.rmeta: src/main.rs src/features/mod.rs src/features/classifier/mod.rs
+/home/android-jester/Documents/Bank-Queuing-System-Project/queuing_server/target/debug/deps/bank_queue_project-441e5fee64634374.rmeta: src/main.rs src/core/mod.rs src/data_sources/mod.rs src/data_sources/classifier/mod.rs src/domain/mod.rs src/presentation/mod.rs
 
-/home/android-jester/Documents/Bank-Queuing-System-Project/target/debug/deps/bank_queue_project-441e5fee64634374.d: src/main.rs src/features/mod.rs src/features/classifier/mod.rs
+/home/android-jester/Documents/Bank-Queuing-System-Project/queuing_server/target/debug/deps/bank_queue_project-441e5fee64634374.d: src/main.rs src/core/mod.rs src/data_sources/mod.rs src/data_sources/classifier/mod.rs src/domain/mod.rs src/presentation/mod.rs
 
 src/main.rs:
-src/features/mod.rs:
-src/features/classifier/mod.rs:
+src/core/mod.rs:
+src/data_sources/mod.rs:
+src/data_sources/classifier/mod.rs:
+src/domain/mod.rs:
+src/presentation/mod.rs:
