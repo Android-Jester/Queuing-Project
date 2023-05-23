b124a3c2110107e48e0dd7b34aff39a7c8ed4cec
Duah Kwadwo Adjei <duah14@outlook.com>
2023-05-23 16:29:04 +0000

    Left with database

--- queuing_server/src/data_sources/remote_source.rs
+++ queuing_server/src/data_sources/remote_source.rs
@@ -1,30 +1,43 @@
 use std::char::MAX;
+use std::fmt::format;
 use std::num::{NonZeroIsize, NonZeroUsize};
 
 use randomforest::{RandomForestClassifier, RandomForestClassifierOptions};
 use randomforest::criterion::{Mse, Gini};
-use randomforest::table::Table;
-use crate::domain::entities::{Teller, Action};
-impl Teller {
-    fn new(teller_id: String, action: Action, average_time: usize, best_time: usize) -> Self {
-        Self {
-            teller_id,
-            current_action: action
+use randomforest::table::{Table, TableBuilder, TableError};
+use crate::domain::entities::{Teller, Action, Customer};
+
+fn train_model(model :&mut RandomForestClassifierOptions, service_time_table: Table) -> RandomForestClassifier {
+    model.fit(Gini, service_time_table)
+}
+
+fn start_model<'a>( max_features :NonZeroUsize, max_samples: NonZeroUsize) -> &'a mut RandomForestClassifierOptions {
+    RandomForestClassifierOptions::new()
+        .seed(10)
+        .max_features(max_features)
+        .max_samples(max_samples)
+}
+
+fn classify_data(service_time_table_builder: &mut TableBuilder, service_time_features: Vec<f64>, service_time_target: f64) -> std::io::Result<()> {
+    if let Ok(data ) = service_time_table_builder.add_row(service_time_features.into(), service_time_target) {
+        Ok(())
+    } else {
+        match TableError {
+            //TODO: Handling Errors on Data acquired from database
+            TableError::EmptyTable => {},
+            TableError::NonFiniteTarget => {},
+            TableError::ColumnSizeMismatch => {}
         }
     }
- 
 }
 
-fn best_line(tellers: Table, features: &[f64], action: String) -> f64 {
-    let MAX_FEATURES = NonZeroUsize::new(12).ok_or(0).unwrap();
-    let MAX_SAMPLES = NonZeroUsize::new(12).ok_or(0).unwrap();
-    let mut random_class: RandomForestClassifier = RandomForestClassifierOptions::new()
-        .seed(10)
-        .max_features(MAX_FEATURES)
-        .max_samples(MAX_SAMPLES)
-        .fit(Gini, tellers);
-    let pred = random_class.predict(features);
-    pred
+fn predict() {
+    // TODO: Obtain Data from database
+    // TODO: Classify Data into target and features
+    // classify_data()
+    // TODO: Train Model
+    // train_model()
+    // TODO: Predict Result
 }
 
-fn change_teller() {}
+
