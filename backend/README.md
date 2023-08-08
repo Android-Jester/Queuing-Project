06116f0a0a193b1d471f86022fb8c4b891539e68
Duah Kwadwo Adjei <duah14@outlook.com>
2023-08-08 17:41:53 +0000

    Tue-Aug-08_17:41:53

--- backend/README.md
+++ backend/README.md
@@ -1,5 +1,30 @@
 # Queuing Modelling System
 
-- This is a system specially designed to enable banks to be able to create adjustable queuing systems
+# User(View)
 
-- This simple tool is designed around making a system realtime and not merely listening to 
+## Auth
+1. Login the user to the service
+2. If user is guest store the data in the guest database
+3. If user is account holder, query his details in the database
+
+## Teller Assignment
+1. User selects the actions they want to perform
+2. Querying the database for the transactions to feed to the random forest algorithm
+3. Determine the position of the user and the teller he/she is assigned to based on least service time
+4. If the user prefers to leave at any point, readjust the queue to exclude the user
+
+# Teller(View)
+## Auth
+1. Login the teller using their id and passcode
+2. Obtain a view of the current users in the queue assigned to him or her
+
+## Activities
+1. Upon servicing the customer, teller clicks completed upon a successful transaction else cancel for unsuccessful transactions
+2. Successful requests are stored in the transactions table in the database
+3. Dispel the user from the queue, and readjust the waiting time
+
+
+
+# Manager(View)
+1. Obtain the analytics of the overall performance of the bank and each of the tellers
+2. Generate a document report based on the data acquired from the analytics
--- backend/src/interface/analytics/actions.rs
+++ backend/src/interface/analytics/actions.rs
@@ -4,10 +4,11 @@ pub async fn daily_report() -> impl Responder {
     let report = get_daily_report();
 
     match report {
-        Ok(transactions) => HttpResponse::Ok().json(transactions),
-        Err(err) => HttpResponse::NotFound().body(err.to_string())
+        Ok(transactions) => HttpResponse::Ok()
+            // .content_type("application/octet-stream")
+            .json(transactions),
+        Err(err) => HttpResponse::NotFound().body(err.to_string()),
     }
-
 }
 
 #[get("/weekly")]
@@ -16,11 +17,9 @@ pub async fn weekly_report() -> impl Responder {
 
     match report {
         Ok(transactions) => HttpResponse::Ok().json(transactions),
-        Err(err) => HttpResponse::NotFound().body(err.to_string())
+        Err(err) => HttpResponse::NotFound().body(err.to_string()),
     }
-
 }
-
 
 #[get("/monthly")]
 pub async fn monthly_report() -> impl Responder {
@@ -28,9 +27,8 @@ pub async fn monthly_report() -> impl Responder {
 
     match report {
         Ok(transactions) => HttpResponse::Ok().json(transactions),
-        Err(err) => HttpResponse::NotFound().body(err.to_string())
+        Err(err) => HttpResponse::NotFound().body(err.to_string()),
     }
-
 }
 
 #[get("/yearly")]
@@ -39,8 +37,6 @@ pub async fn yearly_report() -> impl Responder {
 
     match report {
         Ok(transactions) => HttpResponse::Ok().json(transactions),
-        Err(err) => HttpResponse::NotFound().body(err.to_string())
+        Err(err) => HttpResponse::NotFound().body(err.to_string()),
     }
-
 }
-
